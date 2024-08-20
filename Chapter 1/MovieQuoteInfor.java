67adac43b6cf3178b0ed49614e56c6b2fa29c8bf
MariaNiñaAranas <aranasmarianina@gmail.com>
2024-08-20 18:54:17 +0800

    Create MovieQuoteInfor.java

---
+++ Chapter 1/MovieQuoteInfor.java
@@ -0,0 +1,13 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        String quote = "The first time I saw you, I already liked you.";
+        String movie = "Man In Love";
+        String character = "Zhang Meng-Cheng";
+        int year = 2021;
+
+        System.out.println("Favorite Quote: \"" + quote + "\"");
+        System.out.println("Movie: " + movie);
+        System.out.println("Character: " + character);
+        System.out.println("Year: " + year);
+    }
+}
