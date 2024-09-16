85b172396f8274202328147c2314ed5399302a89
MariaNiñaAranas <aranasmarianina@gmail.com>
2024-09-16 21:01:34 +0800

    Create FirstAddressExercise.java

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,22 @@
+public class FirstAddressExercise {
+   
+   public static void main(String[] args) {
+   System.out.println("First Java Application");
+      displayAddress();
+      }
+      
+      public static void displayAddress() {
+      String add = "SAMSUNG";
+      String address = "Yeongtong-gu";
+      String city = "Suwon";
+      String zipCode = "16222";
+      String country = "South Korea";
+      String companyName = "Samsung Electronics Co., Ltd.";
+      
+      System.out.println(add);
+      System.out.println(address + "," + city + " " + zipCode);
+      System.out.println(country);
+      System.out.println("Company: " + companyName);
+      }
+    }
+      
